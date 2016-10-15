c62b320772b3f68dd2e66282072ac1d8e800f2ac
J. Ryan Conklin <jryanconklin@gmail.com>
2016-10-15 15:34:49 -0700

    Finish Lesson on Inheritance.

--- app/to-do.js
+++ app/to-do.js
@@ -21,9 +21,37 @@ var HomeTask = (function (_super) {
     }
     return HomeTask;
 }(Task));
+var HobbyTask = (function (_super) {
+    __extends(HobbyTask, _super);
+    function HobbyTask(description) {
+        _super.call(this, description, "low"); //The super keyword extends the constructor of the parent class and instructs it to continue creating the class.
+        this.description = description;
+    }
+    return HobbyTask;
+}(Task));
+var WorkTask = (function (_super) {
+    __extends(WorkTask, _super);
+    function WorkTask(dueDate, description, priority) {
+        _super.call(this, description, priority);
+        this.dueDate = dueDate;
+        this.description = description;
+        this.priority = priority;
+    }
+    return WorkTask;
+}(Task));
 var tasks = [];
 tasks.push(new HomeTask("Do the dishes.", "High"));
 tasks.push(new HomeTask("Buy chocolate.", "Low"));
 tasks.push(new HomeTask("Wash the laundry.", "High"));
 tasks[0].markDone();
+tasks.push(new HobbyTask("Practice origami."));
+tasks.push(new HobbyTask("Bake a pie."));
+var today = new Date(); //This uses the JavaScript date built in function to get the current date and store it as a variable.
+var tomorrow = today;
+tomorrow.setDate(today.getDate() + 1);
+var nextDay = today;
+nextDay.setDate(today.getDate() + 2);
+tasks.push(new WorkTask(today, 'Update blog', 'High'));
+tasks.push(new WorkTask(today, 'Go to meeting', 'Medium'));
+tasks.push(new WorkTask(today, 'Clean ceiling', 'Low'));
 console.log(tasks);
--- app/to-do.ts
+++ app/to-do.ts
@@ -25,4 +25,20 @@ tasks.push(new HomeTask("Do the dishes.", "High"));
 tasks.push(new HomeTask("Buy chocolate.", "Low"));
 tasks.push(new HomeTask("Wash the laundry.", "High"));
 tasks[0].markDone();
+
+tasks.push(new HobbyTask("Practice origami."));
+tasks.push(new HobbyTask("Bake a pie."));
+
+var today = new Date(); //This uses the JavaScript date built in function to get the current date and store it as a variable.
+
+var tomorrow = today;
+tomorrow.setDate(today.getDate() + 1);
+
+var nextDay = today;
+nextDay.setDate(today.getDate() + 2);
+
+tasks.push(new WorkTask(today, 'Update blog', 'High'));
+tasks.push(new WorkTask(today, 'Go to meeting', 'Medium'));
+tasks.push(new WorkTask(today, 'Clean ceiling', 'Low'));
+
 console.log(tasks);
