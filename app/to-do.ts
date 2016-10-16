0eae64e7c54319f59c3a606145b6895d0ed4cf48
J. Ryan Conklin <jryanconklin@gmail.com>
2016-10-15 17:03:28 -0700

    Add Interfaces to To Do List App

--- app/to-do.ts
+++ app/to-do.ts
@@ -1,19 +1,29 @@
+interface IPerson {
+  name: string;
+  email: string;
+}
+
 interface ITask {
   description: string;
   done: boolean;
   priority: string;
   markDone(): void;
+  assignedTo?: IPerson;
 }
 
 class Task implements ITask {
   done: boolean = false;
-  constructor (public description: string, public priority: string) {}
+  constructor (public description: string, public priority: string, public assignedTo?: IPerson) {}
   markDone() {
     this.done = true;
   }
 }
 
-class HomeTask extends Task {}
+class HomeTask extends Task {
+  constructor(public description: string, public priority: string, public assignedTo?: IPerson) {
+    super(description, priority);
+  }
+}
 
 class HobbyTask extends Task {
   constructor(public description: string) {
@@ -22,14 +32,29 @@ class HobbyTask extends Task {
 }
 
 class WorkTask extends Task {
-  constructor(public dueDate: Date, public description: string, public priority: string) {
+  constructor(public dueDate: Date, public description: string, public priority: string, public assignedTo?: IPerson) {
     super(description, priority);
   }
 }
 
+var diane: IPerson = {
+  name: "Diane D",
+  email: "diane@epicodus.com"
+}
+
+var thor: IPerson = {
+  name: "Thor Odinson",
+  email: "thor@asgard.com"
+}
+
+var loki: IPerson = {
+  name: "God of mischief",
+  email: "loki@geocities.com"
+}
+
 var tasks: Task[] = [];
 tasks.push(new HomeTask("Do the dishes.", "High"));
-tasks.push(new HomeTask("Buy chocolate.", "Low"));
+tasks.push(new HomeTask("Buy chocolate.", "Low", diane));
 tasks.push(new HomeTask("Wash the laundry.", "High"));
 tasks[0].markDone();
 
@@ -44,8 +69,8 @@ tomorrow.setDate(today.getDate() + 1);
 var nextDay = today;
 nextDay.setDate(today.getDate() + 2);
 
-tasks.push(new WorkTask(today, 'Update blog', 'High'));
-tasks.push(new WorkTask(today, 'Go to meeting', 'Medium'));
-tasks.push(new WorkTask(today, 'Clean ceiling', 'Low'));
+tasks.push(new WorkTask(today, 'Update blog', 'High', diane));
+tasks.push(new WorkTask(today, 'Go to meeting', 'Medium', thor));
+tasks.push(new WorkTask(today, 'Clean ceiling', 'Low', loki));
 
 console.log(tasks);
